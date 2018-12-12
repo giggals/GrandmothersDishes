4cd87d0e4b196de77e64289e53e64d320893d593
giggals <nikolaydddd@abv.bg>
2018-12-12 19:52:56 +0200

    Finished Register , Login and Logout Implementation

--- src/GrandmothersDishes/GrandmothersDishes.Web/Controllers/AccountController.cs
+++ src/GrandmothersDishes/GrandmothersDishes.Web/Controllers/AccountController.cs
@@ -2,48 +2,88 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using GrandmothersDishes.Data;
+using GrandmothersDishes.Models;
+using GrandmothersDishes.Services.GrandmothersDishes.Web.Services.GrandmothersDishes.Users.Contracts;
 using GrandmothersDishes.Web.ViewModels.Account;
 using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
 
 namespace GrandmothersDishes.Web.Controllers
 {
     public class AccountController : Controller
     {
-        //[Authorize]
-        //public async Task<IActionResult> Logout()
-        //{
-        //    await signInManager.SignOutAsync();
-        //    return RedirectToAction("Index", "Home");
-        //}
+        [Authorize]
+        public async Task<IActionResult> Logout()
+        {
+            await signInManager.SignOutAsync();
+            return RedirectToAction("Index", "Home");
+        }
+
+        public AccountController(IGrandmothersDishesUsersService usersService,
+            SignInManager<GrandMothersUser> signInManager)
+        {
+            this.usersService = usersService;
+            this.signInManager = signInManager;
+        }
+
+        private readonly IGrandmothersDishesUsersService usersService;
+
+        private readonly SignInManager<GrandMothersUser> signInManager;
+      
 
         public IActionResult Register()
         {
             return this.View();
         }
 
-        //[HttpPost]
-        //public IActionResult Register(RegisterViewModel model)
-        //{
-        //    //With AutoMapper
-        //    var user = mapper.Map<EventuresUser>(model);
+        [HttpPost]
+        public IActionResult Register(RegisterViewModel model)
+        {
+            //With AutoMapper
+            //var user = mapper.Map<EventuresUser>(model);
 
-        //    //Without AutoMapper 
+            //Without AutoMapper 
 
-        //    //EventuresUser user = new EventuresUser()
-        //    //{
-        //    //    Email = model.Email,
-        //    //    UserName = model.Username
-        //    //};
+            if (!ModelState.IsValid)
+            {
+                return this.View(model);
+            }
 
-        //    IdentityResult result = this.signIn.UserManager.CreateAsync(user, model.Password).Result;
+            GrandMothersUser user = new GrandMothersUser()
+            {
+                UserName = model.Username,
+                City = model.City,
+                HomeAddress = model.HomeAddress,
+                Email = model.Email,
+                
+            };
 
-        //    if (result.Succeeded)
-        //    {
-        //        return this.RedirectToAction("Login", "Account");
-        //    }
+            IdentityResult result = this.signInManager.UserManager.CreateAsync(user, model.Password).Result;
 
-        //    return this.View();
-        //}
+            if (result.Succeeded)
+            {
+                return this.Redirect("/Identity/Account/Login");
+            }
+            else
+            {
+                var usernameExist = this.usersService.AllUsers().FirstOrDefault(x => x.UserName == model.Username);
+                
+                foreach (var error in result.Errors)
+                {
+                    if (usernameExist != null)
+                    {
+                        ModelState.AddModelError("Username", error.Description);
+                    }
+                    else
+                    {
+                        ModelState.AddModelError(string.Empty, "Error accured while trying to register!");
+                    }
+                }
+            }
+
+            return this.View(model);
+        }
     }
 }
