b571e84e22d9892e423189919f4b3adf4e8b0f67
giggals <nikolaydddd@abv.bg>
2018-12-12 19:54:27 +0200

    Middleware for Seeding Roles!

---
+++ src/GrandmothersDishes/GrandmothersDishes.Web/Middlewares/MiddlewareExtensions/MiddlewareExtensions.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Builder;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace GrandmothersDishes.Web.Middlewares.MiddlewareExtensions
+{
+    public static class MiddlewareExtensions
+    {
+        public static IApplicationBuilder UseSeedRolesMiddleware(
+            this IApplicationBuilder builder)
+        {
+            return builder.UseMiddleware<SeedRolesMiddleware>();
+        }
+    }
+}
---
+++ src/GrandmothersDishes/GrandmothersDishes.Web/Middlewares/SeedRolesMiddleware.cs
@@ -0,0 +1,51 @@
+﻿using Microsoft.AspNetCore.Http;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using GrandmothersDishes.Data;
+using GrandmothersDishes.Models;
+using Microsoft.AspNetCore.Identity;
+
+namespace GrandmothersDishes.Web.Middlewares
+{
+    public class SeedRolesMiddleware
+    {
+        private readonly RequestDelegate next;
+
+        public SeedRolesMiddleware(RequestDelegate next)
+        {
+            this.next = next;
+        }
+
+        public async Task InvokeAsync(
+            HttpContext context,
+            GrandmothersDishesDbContext dbContext,
+            UserManager<GrandMothersUser> usermanager,
+            RoleManager<IdentityRole> roleManager)
+        {
+            if (!dbContext.Roles.Any())
+            {
+                await this.SeedRoles(usermanager, roleManager);
+            }
+
+            if (dbContext.Users.Count() == 1)
+            {
+                var firstUser = usermanager.Users.FirstOrDefault();
+                await usermanager.AddToRoleAsync(firstUser, "Administrator");
+            }
+
+            await this.next(context);
+        }
+        
+        private async Task SeedRoles(
+            UserManager<GrandMothersUser> usermanager,
+            RoleManager<IdentityRole> roleManager)
+        {
+            var result = await roleManager.CreateAsync(new IdentityRole("Administrator"));
+           
+        }
+        
+    }
+}
+
